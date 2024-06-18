56f6f2fd1be85cf174e4c5283bd84aed0db42041
Raymond Camden <raymondcamden@gmail.com>
2024-06-18 16:27:04 -0500

    first

---
+++ pdf-embed.js
@@ -0,0 +1,90 @@
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+class PDFEmbed extends HTMLElement {
+
+    constructor() {
+			super();
+
+			this.divid = uuidv4();
+			const wrapper = document.createElement('div');
+			wrapper.id = this.divid;
+			
+			if(this.hasAttribute('url')) this.url = this.getAttribute('url');		
+			if(this.hasAttribute('key')) this.key = this.getAttribute('key');
+
+			if(this.hasAttribute('width')) this.width = this.getAttribute('width');
+			else this.width = '500px';
+
+			if(this.hasAttribute('height')) this.height = this.getAttribute('height');
+			else this.height = '500px';
+
+			this.embedMode = 'FULL_WINDOW';
+			if(this.hasAttributes('embedMode')) this.embedMode =  this.getAttribute('embedMode');
+
+			// if no url, safe to just return?
+			if(!this.url) {
+				console.error('pdf-embed: No url attribute passed.');
+				return;
+			}
+
+			// Ditto for key
+			if(!this.key) {
+				console.error('pdf-embed: No key attribute passed.');
+				return;
+			}
+
+			this.name = this.url.split('/').pop();
+
+			wrapper.style = `width: ${this.width}; height: ${this.height}`;
+			// remove current html
+			this.innerHTML = '';
+			this.parentNode.insertBefore(wrapper, this.nextSibling);
+
+	}
+
+	loadPDF() {
+		var adobeDCView = new AdobeDC.View({clientId: this.key, divId: this.divid });
+		adobeDCView.previewFile({
+			content:{location: {url: this.url }},
+			metaData:{fileName: this.name}
+		}, {
+			embedMode: this.embedMode,
+			focusOnRendering:false
+		});
+
+		adobeDCView.registerCallback(
+			AdobeDC.View.Enum.CallbackType.EVENT_LISTENER,
+			event => {
+				this.dispatchEvent(new CustomEvent(event.type, {
+					composed: true, 
+					bubbles: true, 
+					detail: event.data
+				}));
+			}, { enableFilePreviewEvents: true, enablePDFAnalytics: true, enableAnnotationEvents:true }
+		);
+
+	}
+
+	connectedCallback() {
+		/*
+		the below doesnt work in my test where I have 2 right after each other, but it makes
+		sense, we haven't loaded yet. However, if I use JS to add a new pdf-embed element, in theory, 
+		this optimization will work.
+		*/
+		if(window.AdobeDC) { this.loadPDF(); return; }
+
+		const script = document.createElement('script');
+		script.type = 'text/javascript';
+		script.src='https://documentservices.adobe.com/view-sdk/viewer.js';
+		document.head.appendChild(script);
+
+		if(window.AdobeDC) this.loadPDF();
+		else document.addEventListener('adobe_dc_view_sdk.ready', () => this.loadPDF());
+		
+	}
+
+}
+
+customElements.define('pdf-embed', PDFEmbed);
+
+
---
+++ readme.md
@@ -0,0 +1,31 @@
+# <pdf-embed>
+
+A web component that adds support for Adobe's [PDF Embed API](https://developer.adobe.com/document-services/apis/pdf-embed/). This (free) library lets you add a PDF display to your web page, controlling the size, reading experience and so forth. This web component doesn't expose every feature of the library but should cover most use cases. You can read more about the API [here](https://developer.adobe.com/document-services/docs/overview/pdf-embed-api/).
+
+## Requirements
+
+You must have a client ID created at Adobe's Developer Console. You can sign up [here](https://acrobatservices.adobe.com/dc-integration-creation-app-cdn/main.html?api=pdf-embed-api). Note that the key is domain bound which means it only works on one domain. Also, ote that keys can only have one domain. With that in mind:
+
+* If you want to test on localhost and something else dot com, you will need two keys. 
+* If you set your domain credential to include www, it will not work if someone hits your site without the www. Just specify the domain without any www or some such, and it will work in any subdomain under the initial domain.
+
+## Usage
+
+You must have a URL that points to your PDF. Given that, you can use the library like so:
+
+```html
+<pdf-embed url="https://documentservices.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf"
+		   key="bce5ad180d164236b0998429b39d77cf">
+
+	<p>
+		Read our cool PDF <a href="https://documentservices.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf">here</a>.
+	</p>
+
+</pdf-embed>
+```
+
+That's it.
+
+## Installation
+
+Coming soon.
